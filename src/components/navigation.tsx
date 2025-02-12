d797e37a875e40f214f3000a578bca9073866de1
jovie00 <xianvy0000@gmail.com>
2025-02-12 14:04:23 +0800

    Add current game in nav

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -6,7 +6,9 @@ import { FaDiscord, FaFacebookMessenger, FaGithub } from "react-icons/fa"
 import { useRouter } from 'next/navigation'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import type { User } from '@supabase/auth-helpers-nextjs'
-
+import { GAMESLIST } from '@/lib/data/utils'
+import Image from 'next/image'
+import { Select, SelectTrigger } from './ui/select'
 const Navigation = () => {
   const router = useRouter()
   const supabase = createClientComponentClient()
@@ -49,7 +51,7 @@ const Navigation = () => {
               <span className="font-space-grotesk">Guide</span>
             </Button>
           </div>
-          <div className="flex items-center space-x-4">
+          <div className="flex items-center gap-4">
             <Link href="https://discord.com" target="_blank" className="text-[#B4B7E5] hover:text-white">
               <FaDiscord className="h-4 w-4" />
             </Link>
@@ -59,7 +61,33 @@ const Navigation = () => {
             <Link href="https://github.com" target="_blank" className="text-[#B4B7E5] hover:text-white">
               <FaFacebookMessenger className="h-4 w-4" />
             </Link>
-            {user ? (
+     
+             {!user && (
+             <Button 
+             variant="ghost" 
+             className="text-[#E2E4FF] hover:text-black"
+             onClick={handleSignIn}
+           >
+             <span className="font-space-grotesk">Sign In</span>
+           </Button>
+            )}
+             {user ? (
+            <div className="ml-2">
+            {GAMESLIST.map((game) => (
+             <Select key={game.slug}>
+              <SelectTrigger className=" hover:bg-[#1F2137] text-white flex items-center  gap-2 ">
+                <Image src={game.icon} alt={game.name} width={20} height={20} />
+                <span className="font-space-grotesk text-white">{game.name}</span>
+              </SelectTrigger>
+             </Select>
+            ))}
+            </div>
+            ) : (
+              <Button className="bg-[#4B79E4] hover:bg-[#3D63C9] hidden md:block">
+                <span className="font-space-grotesk">Get Started</span>
+              </Button>
+            )}
+           {user && (
               <Button 
                 variant="ghost" 
                 className="text-[#E2E4FF] hover:text-black"
@@ -67,18 +95,7 @@ const Navigation = () => {
               >
                 <span className="font-space-grotesk">Sign Out</span>
               </Button>
-            ) : (
-              <Button 
-                variant="ghost" 
-                className="text-[#E2E4FF] hover:text-black"
-                onClick={handleSignIn}
-              >
-                <span className="font-space-grotesk">Sign In</span>
-              </Button>
             )}
-            <Button className="bg-[#4B79E4] hover:bg-[#3D63C9] hidden md:block">
-              <span className="font-space-grotesk">Get Started</span>
-            </Button>
           </div>
         </div>
       </div>
