34d19da1367d7c3765e6c60208ce2a50171c928b
xianvy <xianvy0000@gmail.com>
2025-02-14 20:35:19 +0800

    Update hero font size

--- src/features/public/hero.tsx
+++ src/features/public/hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
             <span className="absolute -inset-2 blur-2xl bg-gradient-to-r from-[#4B79E4]/20 to-[#9D68E4]/20" />
 
             {/* Main text with gradient */}
-            <h1 className="text-5xl relative bg-clip-text text-transparent bg-gradient-to-r from-white via-[#E2E4FF] to-[#B4B7E5]">
+            <h1 className="text-2xl sm:text-3xl lg:text-4xl 3xl:text-5xl relative bg-clip-text text-transparent bg-gradient-to-r from-white via-[#E2E4FF] to-[#B4B7E5]">
               Kairos
               <small className="text-[0.65rem] font-normal text-[#B4B7E5] ml-0.5">beta</small>
             </h1>
