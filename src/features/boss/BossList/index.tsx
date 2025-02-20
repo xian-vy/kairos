ae9cbd96c636eaee3f53e07f9839c18c58aa8bbc
jovie00 <xianvy0000@gmail.com>
2025-02-20 11:19:00 +0800

    Update index.tsx

--- src/features/boss/BossList/index.tsx
+++ src/features/boss/BossList/index.tsx
@@ -114,7 +114,7 @@ export function BossList() {
           </Button>
         )}
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {bossData
           .sort((a, b) => a.sortOrder - b.sortOrder)
           .map((boss) => (
