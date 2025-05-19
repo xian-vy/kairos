3b2cbd46b00b5b2155a077d93d84ecd75aaf2b91
xianvy <xianvy0000@gmail.com>
2025-05-19 22:12:06 +0800

    Style : add nav border

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -44,7 +44,7 @@ const Navigation = () => {
   }
   return (
     <>
-      <nav className=" bg-[#0D0F23]/50 backdrop-blur-sm">
+      <nav className="border-b border-[#1F2137] bg bg-[#0D0F23]/50 backdrop-blur-sm">
         <LinearLoading isLoading={isPending} />
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
