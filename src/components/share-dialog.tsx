da403895f8686d339e45617b31ac572a0fd31637
xianvy <xianvy0000@gmail.com>
2025-09-29 03:33:45 +0800

    Refactor :  ShareDialog to use Dialog component

--- src/components/share-dialog.tsx
+++ src/components/share-dialog.tsx
@@ -1,13 +1,9 @@
 'use client'
-import {
-  AlertDialog,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/components/ui/alert-dialog"
 import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+} from "@/components/ui/dialog"
 import { Check, Copy } from "lucide-react"
 import { useState } from "react"
 
@@ -26,34 +22,26 @@ export const ShareDialog = ({ open, onOpenChange }: ShareDialogProps) => {
   }
 
   return (
-    <AlertDialog open={open} onOpenChange={onOpenChange}>
-      <AlertDialogContent className="bg-[#090915] border-[#1F2137] w-11/12 sm:w-full">
-        <AlertDialogHeader>
-          <AlertDialogTitle className="text-[#E2E4FF]"></AlertDialogTitle>
-         
-            <div className="flex items-center gap-2 mt-2">
-              <div className=" p-1 sm:p-2 rounded flex-1 text-[#E2E4FF] text-xs sm:text-sm">
-                kairos-tracker.vercel.app
-              </div>
-              <Button
-                onClick={handleCopyLink}
-                variant="ghost"
-                className="text-[#E2E4FF] hover:text-black !p-2"
-              >
-                {copied ? (
-                  <Check className="h-4 w-4 text-green-500" />
-                ) : (
-                  <Copy className="h-4 w-4" />
-                )}
-              </Button>
-            </div>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel className="bg-[#090915] text-[#B4B7E5] mt-0 hover:bg-[#2A2D4B] hover:text-[#E2E4FF] border-none max-w-[120px] self-end">
-            Close
-          </AlertDialogCancel>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="bg-[#090915] border-[#1F2137] w-11/12 sm:w-full p-8">
+        <div className="flex items-center gap-2 mt-2">
+          <div className="rounded  text-[#E2E4FF] text-xs sm:text-sm">
+            kairos-tracker.vercel.app
+          </div>
+          <Button
+            size="icon"
+            onClick={handleCopyLink}
+            variant="ghost"
+            className="text-[#E2E4FF] hover:text-black"
+          >
+            {copied ? (
+              <Check className="h-4 w-4 text-green-500" />
+            ) : (
+              <Copy className="h-4 w-4" />
+            )}
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
